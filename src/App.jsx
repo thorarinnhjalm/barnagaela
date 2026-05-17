5f3ea9a7129db61d41ae286c026a285000df2a05
Þórarinn Hjálmarsson <thorarinnhjalmarsson@gmail.com>
2026-05-17 20:50:02 +0000

    Hotfix: Add ThemeProvider to App.jsx to fix crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { I18nProvider } from './data/i18n';
 import { AuthProvider } from './data/AuthContext';
+import { ThemeProvider } from './data/ThemeContext';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Landing from './pages/Landing';
@@ -21,37 +22,39 @@ import Terms from './pages/Terms';
 
 export default function App() {
   return (
-    <I18nProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/skilmalar" element={<Terms />} />
-            <Route path="/app" element={<AppShell />}>
-              <Route index element={<Navigate to="/app/gratur" replace />} />
-              <Route path="gratur"   element={<Crying />} />
-              <Route path="svefn"    element={<Sleep />} />
-              <Route path="faeding"  element={<Feeding />} />
-              <Route path="anda"     element={<Breathing />} />
-              <Route path="dagbok"   element={<Diary />} />
-              <Route path="sjalfsum" element={<SelfCare />} />
-              <Route path="maelar" element={<TrackerShell />}>
-                <Route index element={<Navigate to="/app/maelar/faeding" replace />} />
-                <Route path="faeding" element={<FeedingTracker />} />
-                <Route path="svefn"   element={<SleepTracker />} />
-                <Route path="gratur"  element={<CryingTracker />} />
-                <Route path="voxtur"  element={<GrowthTracker />} />
-                <Route path="mynstur" element={<PatternsView />} />
+    <ThemeProvider>
+      <I18nProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/skilmalar" element={<Terms />} />
+              <Route path="/app" element={<AppShell />}>
+                <Route index element={<Navigate to="/app/gratur" replace />} />
+                <Route path="gratur"   element={<Crying />} />
+                <Route path="svefn"    element={<Sleep />} />
+                <Route path="faeding"  element={<Feeding />} />
+                <Route path="anda"     element={<Breathing />} />
+                <Route path="dagbok"   element={<Diary />} />
+                <Route path="sjalfsum" element={<SelfCare />} />
+                <Route path="maelar" element={<TrackerShell />}>
+                  <Route index element={<Navigate to="/app/maelar/faeding" replace />} />
+                  <Route path="faeding" element={<FeedingTracker />} />
+                  <Route path="svefn"   element={<SleepTracker />} />
+                  <Route path="gratur"  element={<CryingTracker />} />
+                  <Route path="voxtur"  element={<GrowthTracker />} />
+                  <Route path="mynstur" element={<PatternsView />} />
+                </Route>
+                <Route path="barn"       element={<BabyProfile />} />
+                <Route path="reikningur" element={<AccountPage />} />
               </Route>
-              <Route path="barn"       element={<BabyProfile />} />
-              <Route path="reikningur" element={<AccountPage />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </AuthProvider>
-    </I18nProvider>
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </AuthProvider>
+      </I18nProvider>
+    </ThemeProvider>
   );
 }
